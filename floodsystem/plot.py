c88dbd457c278673e4e32e2365103a006a4e0709
naseweisssss <ryanlin.x.w@hotmail.co.uk>
2022-02-23 22:02:40 +0000

    Commit for task 2E

---
+++ floodsystem/plot.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+import numpy as np
+
+from .datafetcher import fetch_measure_levels
+
+def plot_water_levels(station, dates, levels):
+    """
+    This function displays a plot of water level data against time for a station, including a plot lines for typical high and low levels
+    """
+
+    plt.plot(dates, levels)
+
+    # Add axis labels, rotate date labels and add plot title
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45)
+    plt.title(station.name)
+    plt.axhline(y=station.typical_range[0], color='r', linestyle='-')
+    plt.axhline(y=station.typical_range[1], color='r', linestyle='-')
+
+
+    # Display plot
+    plt.tight_layout()  # This makes sure plot does not cut off date labels
+    plt.show()
